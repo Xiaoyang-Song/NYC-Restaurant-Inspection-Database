06df3230dd592db4a06bda72d4ee845903034242
root <xs2485@columbia.edu>
2022-10-16 05:31:26 -0400

    Project 1 Restaurant Insertion Demo

--- extract_data.py
+++ extract_data.py
@@ -5,22 +5,21 @@ import numpy as np
 from utils import R_to_col, TOKEN
 
 
-def extract(df, relation_name: str, num_fields=None, default=None):
-    if default is not None and num_fields is not None:
-        assert len(default) == num_fields
+def extract(df, relation_name: str, num_fields:int, default:list):
+    assert len(default) == num_fields
     colname = R_to_col[relation_name]
     data = np.array(df[colname])
     # Replace NA with default values
-    # for i in range(3):
-    #     mask = data[:, i] == TOKEN.NA
-    #     ic(f'{sum(mask)} rows have no {colname[i]}.')
-    #     data[mask, i] = default[i]
+    for i in range(3):
+        mask = data[:, i] == TOKEN.NA
+        ic(f'{sum(mask)} rows have no {colname[i]}.')
+        data[mask, i] = default[i]
     return data
 
 def preprocess(path):
     df = pd.read_csv(path)
     # df = df.fillna(TOKEN.NA)
-    df = df.fillna('NULL')
+    df = df.fillna(TOKEN.NA)
     # Add more if needed
     return df
 
