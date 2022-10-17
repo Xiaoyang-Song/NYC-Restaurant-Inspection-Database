ab2119644b105b9894f3d901e42a6fb04456b035
root <xs2485@columbia.edu>
2022-10-17 18:17:03 -0400

    Test: add_user functionality done

--- users.py
+++ users.py
@@ -8,7 +8,7 @@ def add_user(connection, account_name: str, passcode: str, dob='NULL', district=
     if district != 'NULL':
         assert district in LOCATION_SET
     schema = f"Users(account_name, passcode, dob, district)"
-    cmd = f"INSERT INTO {schema} VALUES({account_name}, {passcode}, {dob_val}, {dis_val})"
+    cmd = f"INSERT INTO {schema} VALUES('{account_name}', '{passcode}', {dob_val}, {dis_val})"
     cursor = connection.execute(cmd)
 
 
