4fb4f7d84241f0284bb24b09ea6a98ac16b482a1
xysong <xysong25@gmail.com>
2022-10-30 16:46:06 -0400

    update schema for users

--- users.py
+++ users.py
@@ -2,13 +2,13 @@ from utils import *
 from extract_data import *
 
 
-def add_user(connection, account_name: str, passcode: str, dob='NULL', district='NULL'):
+def add_user(connection, uid: int, account_name: str, passcode: str, dob='NULL', district='NULL'):
     dob_val = f"'{dob}'" if dob != 'NULL' else 'NULL'
     dis_val = f"'{district}'" if district != 'NULL' else 'NULL'
     if district != 'NULL':
         assert district in LOCATION_SET
-    schema = f"Users(account_name, passcode, dob, district)"
-    cmd = f"INSERT INTO {schema} VALUES('{account_name}', '{passcode}', {dob_val}, {dis_val})"
+    schema = f"Users(userid, account_name, passcode, dob, district)"
+    cmd = f"INSERT INTO {schema} VALUES({uid}, '{account_name}', '{passcode}', {dob_val}, {dis_val})"
     cursor = connection.execute(cmd)
 
 
