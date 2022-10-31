77894949f192566c644560b86ae968cecd99378d
xysong <xysong25@gmail.com>
2022-10-31 15:34:12 -0400

    Registration Update by removing dob

--- frontend/auth.py
+++ frontend/auth.py
@@ -57,7 +57,6 @@ def register():
         userid = request.form['userid']
         account_name = request.form['account_name']
         password = request.form['password']
-        dob = request.form['dob']
         district = request.form['district']
         error = None
 
@@ -68,10 +67,9 @@ def register():
         elif not password:
             error = 'Password is required.'
         # Process optional arguments
-        dob = "NULL" if dob == "" else dob
         if error is None:
             error = add_user(g.conn, userid, account_name,
-                             password, dob, district)
+                             generate_password_hash(password), district)
         if error is None:
             return redirect(url_for("auth.login"))
         flash(error)
